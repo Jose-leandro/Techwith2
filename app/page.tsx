dd7bb3b10fbda6adc1eaec3a84e82ea3cb0fd326
CodeSandbox <support@codesandbox.io>
2025-04-18 18:17:38 +0000

    Sandbox snapshot commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <>
-      <head>
+      <head className="flex justify-around">
         <Image src="/logo" height={150} width={100} alt="Logo Of Website" />
 
         <button>
-          <Image src={""} height={0} width={0} alt="" />
+          <Image src="/icons8-cardápio-48.png" height={20} width={20} alt="" />
         </button>
 
         <h1>Get Us Services</h1>
