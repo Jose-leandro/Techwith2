531d17230c923c91b2965137e3b0bbc957af3ea5
CodeSandbox <support@codesandbox.io>
2025-04-25 18:26:56 +0000

    Sandbox snapshot commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,23 +2,26 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white text-center px-4">
-      <h1 className="text-8xl w-4/6 sm:text-5xl font-bold mb-4">
-        <span className="">TechWith2</span> – Landing Pages That Convert, Code
-        That Performs.
-      </h1>
+    <section className="min-h-screen flex justify-around items-center bg-gray-900 text-white text-center px-4">
+      <div className="flex flex-col items-start w-auto">
+        <a
+          href="#contact"
+          className="px-6 py-3 text-black bg-white mb-1 rounded-xl font-medium shadow-lg transition-all"
+        >
+          Let’s Build Your Page
+        </a>
+
+        <span className="text-9xl">TechWith2</span>
+
+        <h1 className="text-7xl w-[700px] sm:text-5xl font-bold mb-4">
+          – Landing Pages That Convert, Code That Performs.
+        </h1>
+      </div>
 
       <p className="text-lg sm:text-xl text-gray-300 max-w-2xl mb-8">
         We build high-performing, visually striking landing pages that bring
         your ideas to life and generate real results.
       </p>
-
-      <a
-        href="#contact"
-        className="px-6 py-3 bg-gradient-to-r from-purple-500 to-green-400 text-white rounded-xl font-medium shadow-lg transition-all"
-      >
-        Let’s Build Your Page
-      </a>
     </section>
   );
 }
