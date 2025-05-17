d755e997c349acf9aff378ce0f36895c0498e4d6
CodeSandbox <support@codesandbox.io>
2025-05-17 16:10:01 +0000

    Sandbox snapshot commit

--- app/components/our-services.tsx
+++ app/components/our-services.tsx
@@ -26,27 +26,32 @@ export default function OurServices() {
 
   return (
     <section>
-      <div>
+      <div className="flex justify-between">
         <h2 className="text-5xl font-abril mt-5 ml-3">
           Our <span className="text-7xl font-abril">Services</span>
         </h2>
 
-        <button>Get US Services</button>
+        <button className="mr-3 rounded-sm h-7 w-auto px-3 bg-zinc-100 text-black">
+          Get US Services
+        </button>
       </div>
 
-      <p>
+      <p className="mt-2 ml-2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia inventore
         atque libero nisi quisquam vitae ipsam.
       </p>
 
-      <div className="flex just">
+      <div className="flex justify-around mt-6">
         {OurServices.map((service) => (
-          <div key={service.id} className="">
+          <div
+            key={service.id}
+            className="bg-black rounded-2xl mb-2 flex flex-col justify-evenly"
+          >
             <Image
               src={service.imageUrl}
               alt={service.title}
-              width={200}
-              height={150}
+              width={40}
+              height={30}
             />
             <h3>{service.title}</h3>
             <p>{service.description}</p>
