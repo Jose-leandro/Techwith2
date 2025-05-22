6a4708784308a6c4da571e74b49b83dfd0187c53
CodeSandbox <support@codesandbox.io>
2025-05-22 16:20:00 +0000

    Sandbox snapshot commit

--- app/components/our-promotion-for-you.tsx
+++ app/components/our-promotion-for-you.tsx
@@ -8,12 +8,14 @@ export default function OurPromotionForYou() {
       title: "Free + Feedback",
       description:
         " Dolore reiciendis ea corrupti libero dolor facere dignissimos ex, nemo beatae velit a rerum?",
+      style: "flex-col",
     },
     {
       id: 2,
       title: "By 90% Off",
       description:
         " Dolore reiciendis ea corrupti libero dolor facere dignissimos ex, nemo beatae velit a rerum!",
+      style: "flex-col-reverse",
     },
   ];
   return (
@@ -23,16 +25,21 @@ export default function OurPromotionForYou() {
         <span className="text-7xl font-abril">Your</span>
       </h2>
 
-      <div className="flex justify-around flex-col mt-6">
+      <div className="flex justify-around mt-6">
         {OurPomotion.map((service) => (
-          <div key={service.id}>
-            <div className="bg-black rounded-2xl px-6 py-7 w-2/5 mb-2 h-auto flex flex-col items-start justify-evenly">
-              <h3 className="text-4xl mt-1">{service.title}</h3>
-              <button className="bg-white text-black">
-                Get This Promotion
-              </button>
+          <div className={`flex ${service.style} items-center`}>
+            <h3 className="text-4xl mt-1 mb-2">{service.title}</h3>
+
+            <div key={service.id} className="flex items-center justify-center">
+              <div className="bg-gray-300 rounded-2xl px-6 py-7 w-2/5 mb-2 h-auto flex flex-col items-start justify-evenly lg:w-4/5">
+                <p className="text-1xs my-3 mb-2 text-black">
+                  {service.description}
+                </p>
+                <button className="bg-blue-900 text-white py-2 px-4 rounded-sm">
+                  Get This Promotion
+                </button>
+              </div>
             </div>
-            <p className="text-xs my-3 mb-2">{service.description}</p>
           </div>
         ))}
       </div>
