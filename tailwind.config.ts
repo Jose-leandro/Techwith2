a795b1bf6d100c349bb886ab9b3efe41f5de8e65
José Leandro <leandrojose124leandro@gmail.com>
2025-08-14 17:36:52 -0300

    chore: set up prettier, reset eslint config and install dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
       fontFamily: {
-        abril: ['"Abril Fatface"', "serif"],
-        garamond: ['"EB Garamond"', "serif"],
-        ibmplex: ['"IBM Plex Serif"', "serif"],
+        abril: ['"Abril Fatface"', 'serif'],
+        garamond: ['"EB Garamond"', 'serif'],
+        ibmplex: ['"IBM Plex Serif"', 'serif'],
       },
     },
   },
